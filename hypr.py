8b6f1dfdc4c29f65d65acbf3d5cd8f659d6d1971
bbnoir <bbnoir83@gmail.com>
2023-09-05 18:29:55 +0800

    rcan more data - 31.392dB - 287083

--- hypr.py
+++ hypr.py
@@ -2,12 +2,12 @@ import torch
 
 config = {
     "data_dir": "./data/sorted",
-    "data_split_num": 800,
-    "data_num": 900,
+    "data_split_num": 3300,
+    "data_num": 3550,
     "device": torch.device("cuda" if torch.cuda.is_available() else "cpu"),
     # you can set your own training configurations
     "batch_size": 1,
     "learning_rate": 1e-3,
-    "epoch_num": 30,
+    "epoch_num": 40,
     "resume": False,
 }
